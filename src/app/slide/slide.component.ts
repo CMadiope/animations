1af4e2815382331abeb11ad3b745ec0277576492
Confidence Madiope <confidencem97@gmail.com>
2022-07-24 21:29:57 +0200

    ADD: animation functions to slide component

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 @Component({
   selector: 'app-slide',
@@ -10,6 +12,19 @@ export class SlideComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger)
+
+    gsap.utils.toArray('section').forEach((section: any, i) => {
+      section.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight}?random=${i})`
+
+      gsap.to(section, {
+        scrollTrigger:{
+          trigger: section,
+        pin: true,
+        pinSpacing: false
+        }
+      })
+    })
   }
 
 }
